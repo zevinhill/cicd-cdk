8d711131a239891ca072bdff72c0962b1823cd48
zevinhill <zevin.hill@gmail.com>
2023-11-13 17:32:59 +0100

    initial commit

---
+++ bin/demoawspipeline.ts
@@ -0,0 +1,9 @@
+#!/usr/bin/env node
+import 'source-map-support/register';
+import * as cdk from 'aws-cdk-lib';
+import { DemoawspipelineStack } from '../lib/demoawspipeline-stack';
+
+const app = new cdk.App();
+new DemoawspipelineStack(app, 'DemoawspipelineStack', {
+  env: { account: '408177141670' , region: 'eu-central-1' },
+});
