6724721e79b298c9a179379f2433d6b820fcb908
zevinhill <zevin.hill@gmail.com>
2023-11-13 17:57:32 +0100

    fixed error in code

--- lib/demoawspipeline-stack.ts
+++ lib/demoawspipeline-stack.ts
@@ -1,8 +1,7 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-import * as pipelines from 'aws-cdk-lib/pipelines';
-import { CodePipeline } from 'aws-cdk-lib/aws-events-targets';
-import { CodePipelineSource } from 'aws-cdk-lib/pipelines';
+//import * as pipelines from 'aws-cdk-lib/pipelines';
+import { CodePipeline, CodePipelineSource, ShellStep } from 'aws-cdk-lib/pipelines';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class DemoawspipelineStack extends cdk.Stack {
@@ -10,9 +9,9 @@ export class DemoawspipelineStack extends cdk.Stack {
     super(scope, id, props);
 
     // The code that defines your stack goes here
-    const democicdpipeline = new pipelines.CodePipeline(this, 'demoPipeline', 
+    const democicdpipeline = new CodePipeline(this, 'demoPipeline', 
     {    
-      synth: new pipelines.ShellStep('Synth', {
+      synth: new ShellStep('Synth', {
         // Use a connection created using the AWS Console to authenticate to GitHub
         // Other sources are available
         input: CodePipelineSource.gitHub('zevinhill/cicd-cdk', 'main'),
